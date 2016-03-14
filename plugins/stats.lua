76912a1ac75cffc3d6a5a44b1354b6c7d4bc8bd1
vintonrobot <vintonrobot@gmail.com>
2016-03-14 19:25:51 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (vinton)",-- Put everything you like :)
+		"^[!/]([Vv]inton)"-- Put everything you like :)
     }, 
   run = run
 }
